972e29caaf41f2c13eef40ee6c504480b3dc80aa
BulatTH <bulatxajrutdinoff@yandex.ru>
2019-02-19 14:56:35 +0300

    Recalc goods prices

--- app/views/Main/index.php
+++ app/views/Main/index.php
@@ -47,6 +47,7 @@
         <div class="product-top">
             <div class="product-one">
                 <?php if ($hits): ?>
+                <?php $curr = \ishop\App::$app->getProperty("currency");?>
                     <?php foreach ($hits as $hit): ?>
                         <div class="col-md-3 product-left">
                             <div class="product-main simpleCart_shelfItem">
@@ -55,9 +56,18 @@
                                     <h3><a href="product/<?= $hit->alias ?>"> <?= $hit->title ?> </a> </h3>
                                     <p>Explore Now</p>
                                     <h4>
-                                        <a class="add-to-cart-link" href="cart/add?id=<?= $hit->id ?>"><i></i></a> <span class=" item_price">$ <?= $hit->price ?></span>
+                                        <a class="add-to-cart-link" href="cart/add?id=<?= $hit->id ?>"><i></i></a> <span class=" item_price">
+                                            <?= $curr['symbol_left'] ?>
+                                                <?= $hit->price * $curr['value'] ?></span>
+                                            <?= $curr['symbol_right'] ?>
                                         <?php if ($hit->old_price): ?>
-                                            <small><del> <?= $hit->old_price ?> </del></small>
+                                            <small>
+                                                <del>
+                                                    <?= $curr['symbol_left'] ?>
+                                                        <?= $hit->old_price * $curr['value'] ?>
+                                                    <?= $curr['symbol_right'] ?>
+                                                </del>
+                                            </small>
                                         <?php endif; ?>
                                     </h4>
                                 </div>

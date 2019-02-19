409024af3a448d1fd1fade13d779146363cb2b47
BulatTH <bulatxajrutdinoff@yandex.ru>
2019-02-19 11:34:25 +0300

    Hit products (main/index)

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -22,9 +22,10 @@ class MainController extends AppController
     {
         // App::$app->getProperty('shop_name')
         $brands = R::find('brand', " LIMIT 3");
+        $hits = R::find('product', "hit = '1' AND status = '1' LIMIT 8");
 
         $this->setMeta( 'Главная страница', "Описание", "Ключи");
 
-        $this->set(compact('brands'));
+        $this->set(compact('brands', 'hits'));
     }
 }

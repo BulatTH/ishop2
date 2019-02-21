b18ed7a7a5df97c4254623690876a9cc0d614d5a
BulatTH <bulatxajrutdinoff@yandex.ru>
2019-02-21 11:17:48 +0300

    Pagination

--- app/views/Category/view.php
+++ app/views/Category/view.php
@@ -3,8 +3,7 @@
     <div class="container">
         <div class="breadcrumbs-main">
             <ol class="breadcrumb">
-                <li><a href="<?= PATH ?>"> Main </a></li>
-                <li> Категория </li>
+                <?= $breadcrumbs ?>
             </ol>
         </div>
     </div>
@@ -42,8 +41,15 @@
                                 </div>
                             </div>
                         <?php endforeach; ?>
+                        
+                        <div class="clearfix"></div>
 
-
+                        <div class="text-center">
+                            <p> (<?= count($products) ?> товара(ов) из <?= $total ?>) </p>
+                            <?php if ($pagination->countPages > 1): ?>
+                                <?= $pagination ?>
+                            <?php endif; ?>
+                        </div>
                         <div class="clearfix"></div>
                     </div>
                 <?php else: ?>
